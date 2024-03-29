2943b4193248e59cf2eced4d8c5a851890ec1057
griff182uk <griff182uk@yahoo.co.uk>
2024-03-29 17:00:35 +0000

    added exit code

--- test/test_simple.sh
+++ test/test_simple.sh
@@ -1,7 +1,10 @@
+set -e  # Exit immediately if a command exits with a non-zero status.
+
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5240/weatherforecast)
 
 if [ "$response" -eq 200 ]; then
     echo "Success: HTTP status code is 200"
 else
     echo "Error: HTTP status code is $response"
+    exit 1
 fi
