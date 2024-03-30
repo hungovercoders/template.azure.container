f3a4aa09d2965dd3a75aae5f625b078a534e89dc
griff182uk <griff182uk@yahoo.co.uk>
2024-03-30 15:37:34 +0000

    added requirements script

--- test/tests.sh
+++ test/tests.sh
@@ -1,6 +1,7 @@
 # set -e  # Exit immediately if a command exits with a non-zero status.
 
 echo "Starting script: $0..."
+
 echo "Starting tests..."
 sh ../test/simple_test.sh
 echo "Completed script: $0."
